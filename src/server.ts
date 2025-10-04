575b1091fe60331504ceda915d18610ec1224b9c
jdotglove <glover.jarod@gmail.com>
2025-10-04 16:43:05 -0500

    TGD-01 additional attempt to fix routing

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "./plugins/express";
-import path from "path";
+import * as path from "path";
 import router from "./routes";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
@@ -8,31 +8,33 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "../public")));
 app.use(cookieParser());
 app.use('/api', router);
 
 
 app.get("/knowledge-hub", (req: Request, res: Response) => {
     console.log("Serving knowledge-hub.html");
-    res.sendFile(path.join(__dirname, "../public/knowledge-hub.html"));
+    res.sendFile(path.resolve(__dirname, "../public", "knowledge-hub.html"));
 })
 
 app.get("/login", (_req: Request, res: Response) => {
     console.log("Serving login.html");
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.resolve(__dirname, "../public", "login.html"));
 });
 
 app.get("/create-admin", (_req: Request, res: Response) => {
     console.log("Serving create-admin.html");
-    res.sendFile(path.join(__dirname, "../public/create-admin.html"));
+    res.sendFile(path.resolve(__dirname, "../public", "create-admin.html"));
 });
 
 app.get("/", (_req: Request, res: Response) => {
     console.log("Serving index.html");
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.resolve(__dirname, "../public", "index.html"));
 });
 
-app.use(express.static(path.join(__dirname, "../public")));
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);

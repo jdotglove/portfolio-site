e3c0ce30abce7d533eefff31cf64386e235b3914
jdotglove <glover.jarod@gmail.com>
2025-10-05 13:26:02 -0500

    TGD-01: handle trailing slashes

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,12 +1,12 @@
-import express from '../../plugins/express';
+import express from "../../plugins/express";
 import {
     adminLogin,
     createAdmin,
-} from './handlers';
+} from "./handlers";
 
 const router = express.Router();
 
-router.post('/login', adminLogin);
-router.post('/create', createAdmin);
+router.post(["/login", "/login/"], adminLogin);
+router.post(["/create", "/create/"], createAdmin);
 
 export default router;

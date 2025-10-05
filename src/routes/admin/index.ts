1b1545bf94664299f5f6b2d57dd877e2a100e41b
jdotglove <glover.jarod@gmail.com>
2025-10-05 13:59:46 -0500

    TGD-01: switch axios implementation

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -6,7 +6,9 @@ import {
 
 const router = express.Router();
 
-router.post(["/login", "/login/"], adminLogin);
-router.post(["/create", "/create/"], createAdmin);
+router.post("/login", adminLogin);
+router.post("/login/", adminLogin);
+router.post("/create", createAdmin);
+router.post("/create/", createAdmin);
 
 export default router;

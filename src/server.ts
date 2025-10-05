406bbe94f47eab2a9da901c4bdecc47874b0ad2a
jdotglove <glover.jarod@gmail.com>
2025-10-04 19:43:44 -0500

    TGD-01: fix api route handling

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, "../public")));
 app.use(cookieParser());
-app.use('/api', router);
+app.use("/api", router);
 
 app.get("/", (_req: Request, res: Response) => {
     console.log("Serving index.html");
@@ -31,6 +31,17 @@ app.get("/login", (_req: Request, res: Response) => {
 app.get("/create-admin", (_req: Request, res: Response) => {
     console.log("Serving create-admin.html");
     res.sendFile(path.resolve(__dirname, "../public", "create-admin.html"));
+});
+
+// Graceful shutdown for App Runner
+process.on('SIGTERM', () => {
+    console.log('SIGTERM received, shutting down gracefully');
+    process.exit(0);
+});
+
+process.on('SIGINT', () => {
+    console.log('SIGINT received, shutting down gracefully');
+    process.exit(0);
 });
 
 app.listen(port, () => {

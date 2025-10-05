a9ce57d13e030350cf6a4082e838d054ec591c01
jdotglove <glover.jarod@gmail.com>
2025-10-05 14:11:10 -0500

    update api handling

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -7,8 +7,6 @@ import {
 const router = express.Router();
 
 router.post("/login", adminLogin);
-router.post("/login/", adminLogin);
 router.post("/create", createAdmin);
-router.post("/create/", createAdmin);
 
 export default router;
